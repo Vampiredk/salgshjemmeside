445ae8a8f082efcb9cc3553c1755f2807b203c39
Vampiredk <simonbreit2@gmail.com>
2022-05-10 15:34:19 +0200

    Salgshjemmeside

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div id="topbar">
+      <div id="topbarcolour">
+      </div>
+      <div class="topbarcontent">
+        <div class="topbarcontentpos">
+          <router-link to="/">
+            <button class="topbarbtn">Home</button>
+          </router-link>
+          <router-link to="/about">
+            <button class="topbarbtn">about</button>
+          </router-link>
+          <div class="topbarcontentright">
+            <button class="topbarbtn">kurv</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
