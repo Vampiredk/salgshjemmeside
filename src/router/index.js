db919002f7e1bdc46cb2b2945863deee5827da18
AndreasData <alykkehenriksen@gmail.com>
2022-06-23 15:04:37 +0200

    Loging test

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  connectAuthgEmulator,
+  signInWithEmailAndPassword
+} from 'firebase/auth'
+import { showLoginError } from './Firebase'
 const routes = [
   {
     path: '/',
@@ -36,4 +42,33 @@ const router = createRouter({
   routes
 })
 
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyDUKCYyOm_MsCXGe0BG6cWUQyH0Sopu0lg',
+  authDomain: 'salgssidelog.firebaseapp.com',
+  projectId: 'salgssidelog',
+  storageBucket: 'salgssidelog.appspot.com',
+  messagingSenderId: '1040433312578',
+  appId: '1:1040433312578:web:132e802a58808902cf8edb',
+  measurementId: 'G-982TQCJQZL'
+})
+
+const auth = getAuth(firebaseApp)
+
+connectAuthgEmulator(auth, 'http://localhost:8080/')
+
+
+const logInEmailPassword = async () =>{
+  const loginEmail = txtEmail.value
+  const loginPassword = txtPassword.value
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+    console.log(userCredential.user)
+  }
+  catch(error) {
+    console.log(error)
+    showLoginError(error)
+  }
+  
+}
 export default router
